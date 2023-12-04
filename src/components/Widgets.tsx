5962109c6f6645cde0885fa15ec021a60ad12c27
Mohammed Falah <falahsss900@gmail.com>
2023-12-04 16:26:02 +0530

    bot text

--- src/components/Widgets.tsx
+++ src/components/Widgets.tsx
@@ -13,14 +13,12 @@ const Widgets = () => {
         <Script src="../widget/coachbots-widget.js"/>
         <Script src="../widget/coachbots-stt-widget.js"/>
 
-        <div
-          className={`fixed max-sm:left-[2.5rem] left-[2rem] bottom-28 hover:cursor-pointer max-sm:bottom-[5.5rem] w-[10%]`}
-        >
-          <p className="text-xs max-sm:hidden"><span className="font-bold">CoachScribe</span><span> is our high performance bot but speech analytics is not available.</span></p>
+        <div className="fixed max-sm:left-[1.6rem] left-[2rem] bottom-28 hover:cursor-pointer max-sm:bottom-[5.5rem] w-[10%] max-sm:w-[30%]">
+          <p className="text-xs text-left"><span className="font-bold max-sm:text-[10px] max-sm:relative max-sm:p-1 rounded-lg max-sm:bg-[#35DDB8] w-fit ">CoachScribe </span><span className="max-sm:hidden"> is our high performance bot but speech analytics is not available.</span></p>
         </div>
 
-        <div className="fixed max-sm:right-[2.8rem] right-[2rem] bottom-28 hover:cursor-pointer max-sm:bottom-[5.5rem] w-[10%]">
-          <p className="text-xs text-right max-sm:hidden"><span className="font-bold">CoachTalk</span><span> is our medium performance bot that includes speech analytics.</span></p>
+        <div className="fixed max-sm:right-[1.8rem] right-[2rem] bottom-28 hover:cursor-pointer max-sm:bottom-[5.5rem] w-[10%] max-sm:w-[30%]">
+          <p className="text-xs text-right"><span className="font-bold max-sm:text-[10px] max-sm:relative max-sm:p-1 rounded-lg max-sm:bg-[#35DDB8] w-fit ">CoachTalk </span><span className="max-sm:hidden"> is our medium performance bot that includes speech analytics.</span></p>
         </div>
 
         <div
