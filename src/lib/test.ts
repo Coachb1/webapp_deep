320677ed7ededb040505ddba5a4f707a8ed9313f
Mohammed Falah <falahsss900@gmail.com>
2023-12-27 16:58:54 +0530

    added another fix to image hover

--- src/lib/test.ts
+++ src/lib/test.ts
@@ -468,7 +468,7 @@ export const coachingPlus: Test[] = [
     interaction_mode: "Audio",
   },
   {
-    domain: "Life Coaching",
+    domain: "Manager Coaching",
     test_code: "QFN8OWT",
     title: "Counselling session for stress management.",
     description:
