071139d5ac9a038eafc745fb390d498b831d3258
Mohammed Falah <falahsss900@gmail.com>
2024-01-06 18:14:33 +0530

    added mods to coach

--- src/app/Coach.tsx
+++ src/app/Coach.tsx
@@ -12,6 +12,7 @@ import { Button } from "@/components/ui/button";
 import {
   AlertTriangle,
   BookmarkCheck,
+  CornerDownRight,
   FileClock,
   LibraryBig,
   Loader,
@@ -39,6 +40,10 @@ const Coach = ({ user, renderType }: any) => {
   const [coachName, setCoachName] = useState<string>("");
   const [coachDescription, setCoachDescription] = useState<string>("");
   const [isLoading, setIsLoading] = useState(true);
+  const [coachProfileLink, setCoachProfileLink] = useState(
+    "https://www.linkedin.com/"
+  );
+  const [coachBookLink, setCoachBookLink] = useState("https://calendly.com/");
 
   const [invalidId, setInValidCoach] = useState(false);
 
@@ -85,46 +90,33 @@ const Coach = ({ user, renderType }: any) => {
     {
       heading: "Sessions Orientation",
       description:
-        "Prepare for your session! Access valuable information about your coach/mentor before your session, providing a clear picture of their expertise and style.",
+        "Prepare for your session! Access valuable information about your coach/mentor before your session, clearly showing their expertise and style.",
     },
     {
-      heading: "Interim Conversation",
+      heading: "Check-in Conversation",
       description:
         "Stay connected between sessions! Engage in interim conversations, gaining insights and guidance whenever you need it.",
+    },
+    {
+      heading: "Recommendations",
+      description:
+        "The Avatar may also have the ability to recommend scenarios to practice based on the needs. The user may also rely directly search the library to search for relevant scenarios for practice.",
     },
   ];
 
   const benefitsData = [
     {
-      heading: "Transcription Email",
-      icon: (
-        <MailPlus
-          className="h-8 w-8 max-sm:h-6 max-sm:w-6 mb-2 stroke-gray-600"
-          stroke="10"
-        />
-      ),
+      heading: "Transcript Email",
       description:
-        "Never miss a detail! Receive a transcription email after each session, capturing key insights and action points for easy reference. There is always a mentor/coach oversight in the conversation based on emails.",
+        "Never miss a detail! Receive a transcription email after each session, capturing key insights and action points for easy reference.",
     },
     {
-      heading: "Advice Anytime",
-      icon: (
-        <FileClock
-          className="h-8 w-8 max-sm:h-6 max-sm:w-6 mb-2 stroke-gray-600"
-          stroke="10"
-        />
-      ),
+      heading: "Advice Anytime, Anywhere",
       description:
-        " Your coach is always with you! Receive coaching/mentoring advice from the bot anytime, anywhere—empowering you to excel in both personal and professional endeavors.",
+        "Your coach is always with you! Receive coaching/mentoring advice from the bot anytime, anywhere—empowering you to excel in personal and professional endeavors.",
     },
     {
       heading: "Skill scenario library",
-      icon: (
-        <LibraryBig
-          className="h-8 w-8 max-sm:h-6 max-sm:w-6 mb-2 stroke-gray-600"
-          stroke="10"
-        />
-      ),
       description:
         "Explore our extensive library of skill scenarios. This collection provides practical, real-life situations for skill development. Enhance your skills by tackling scenarios that resonate with the needs identified during the session.",
     },
@@ -132,11 +124,20 @@ const Coach = ({ user, renderType }: any) => {
 
   return (
     <>
+      {renderType === "static" && (
+        <Script src="../widget/coachbots-stt-widget.js" />
+      )}
       <Script
         src="https://static.elfsight.com/platform/platform.js"
         data-use-service-core
         defer
       />
+
+      <div className="fixed max-sm:hidden right-[100px] bottom-12">
+        <span className="mr-6 text-sm font-bold">Try Now</span>
+        <CornerDownRight className="ml-4 h-12 w-12 text-gray-600" />
+      </div>
+
       <div
         className="elfsight-app-a2ca2565-f013-4a6a-9ad8-3ff1f7eadf9a"
         data-elfsight-app-lazy
@@ -169,24 +170,71 @@ const Coach = ({ user, renderType }: any) => {
             BOTS
           </h1>
           <div>
-            <h1 className="text-5xl mt-0 font-bold md:text-6xl lg:text-4xl text-gray-700 max-sm:text-3xl max-sm:px-4 bg-gradient-to-br from-gray-700 to-gray-400 bg-clip-text text-transparent">
+            <h1 className="text-5xl mt-0 font-bold md:text-6xl lg:text-4xl  max-sm:text-2xl text-gray-600 ">
               {renderType === "dynamic"
-                ? `Welcome to ${coachName}'s Avatar`
-                : "Welcome to Your Coach's Avatar"}
+                ? `Welcome to ${coachName}'s Avatar🚀`
+                : "Welcome to the Coach, Mentor Avatar!🚀"}
             </h1>
-            <p className="my-4 max-sm:text-xs text-[#7f7f7f]">
-              {renderType === "dynamic"
-                ? coachDescription
-                : "This is your coach/mentor's personalized bot. Here, you would typically find a detailed description of your coach/mentor—highlighting their expertise, approach, and unique coaching/mentoring style. Dive into the detailed sections to explore the benefits and learn how it all works. Our bot is trained on the coach/ mentor's style, ideologies, and coaching/mentoring style, ensuring a tailored and effective coaching experience."}
+            <p className="my-4 max-sm:text-xs text-[#2f2323]">
+              <div className="p-2 border border-gray-200 bg-blue-100 rounded-lg">
+                {" "}
+                This is your coach/mentor’s personalized bot. Here, you would
+                typically find a detailed description of your
+                coach/mentor—highlighting their expertise, approach, and unique
+                coaching/mentoring style. Dive into the detailed sections to
+                explore the benefits and learn how it works. Our bot is trained
+                on the coach/ mentor’s style, ideologies, and coaching/mentoring
+                style, ensuring a tailored and effective coaching experience.{" "}
+              </div>
             </p>
+            {renderType !== "dynamic" && (
+              <p className="my-4 max-sm:text-xs text-[#2f2323]">
+                This is where you will see the summary information of the
+                particular coach avatar. The bot on this page demonstrates a
+                conversation based on this profile.
+              </p>
+            )}
+            {renderType === "dynamic" ? (
+              coachDescription
+            ) : (
+              <p className="max-sm:text-xs text-[#2f2323]">
+                <b>Sample</b> : I'm Aarav Sharma, a seasoned corporate coach
+                with 15+ years' experience in leadership development. Holding a
+                master's in organizational psychology and certifications in
+                executive coaching, I've collaborated with top-tier companies.
+                My coaching style, a unique blend of empathy and strategic
+                thinking, fosters a growth mindset and aligns personal values
+                with professional goals. Known for approachability, I create a
+                safe space for executives, incorporating mindfulness for
+                self-awareness and resilience. Tailoring strategies to
+                individual needs, I aim to be a trusted guide for long-term,
+                sustainable leadership development in the dynamic corporate
+                landscape.
+              </p>
+            )}
           </div>
-          <div className="flex flex-row gap-2">
+          <div className="flex flex-row gap-2 flex-wrap mt-8 max-sm:items-center max-sm:justify-center">
+            <Link target="_blank" href={coachProfileLink}>
+              <div className="relative group cursor-pointer">
+                <div className="absolute -inset-1 bg-gradient-to-r from-red-600 to-violet-600 rounded-lg blur opacity-25 group-hover:opacity-40 transition duration-1000 group-hover:duration-200"></div>
+                <div className="relative bg-white ring-1 ring-gray-900/5 rounded-lg leading-none flex items-top justify-start space-x-6">
+                  <div className="space-y-2">
+                    <Button
+                      variant={"secondary"}
+                      className="border border-gray-200 h-8 hover:cursor-pointer w-fit"
+                    >
+                      Profile {renderType !== "dynamic" && "(sample)"}
+                    </Button>
+                  </div>
+                </div>
+              </div>
+            </Link>
             <Link href={"#howItWorks"}>
               <Button
                 variant={"secondary"}
                 className="border border-gray-200 h-8 hover:cursor-pointer"
               >
-                How it works
+                How Avatar works
               </Button>
             </Link>
             <Link href={"#benefits"}>
@@ -197,6 +245,21 @@ const Coach = ({ user, renderType }: any) => {
                 Benefits
               </Button>
             </Link>
+            <Link target="_blank" href={coachBookLink}>
+              <div className="relative group cursor-pointer">
+                <div className="absolute -inset-1 bg-gradient-to-r from-red-600 to-violet-600 rounded-lg blur opacity-25 group-hover:opacity-40 transition duration-1000 group-hover:duration-200"></div>
+                <div className="relative bg-white ring-1 ring-gray-900/5 rounded-lg leading-none flex items-top justify-start space-x-6">
+                  <div className="space-y-2">
+                    <Button
+                      variant={"secondary"}
+                      className="border border-gray-200 h-8 hover:cursor-pointer"
+                    >
+                      Book me {renderType !== "dynamic" && "(sample)"}
+                    </Button>
+                  </div>
+                </div>
+              </div>
+            </Link>
           </div>
 
           <div className="w-full" id="howItWorks">
@@ -205,7 +268,7 @@ const Coach = ({ user, renderType }: any) => {
                 variant={"secondary"}
                 className="bg-[#2DC092] z-10 h-6 w-fit text-white text-lg py-3 hover:bg-[#2DC092] text-center mb-8 mt-12 max-sm:mt-8 max-sm:text-sm"
               >
-                How it works
+                How Avatar works
               </Badge>
             </div>
             <div className="w-full">
@@ -303,7 +366,8 @@ const Coach = ({ user, renderType }: any) => {
               The coach/mentor's personalized bot is designed to enhance your
               coaching/mentoring experience. The information provided in the
               coach/mentor's detailed sections serves as a guide, and the
-              effectiveness of coaching/mentoring is subjective.
+              effectiveness of coaching/mentoring is subjective. The coach can
+              override the discussion via email.
             </p>
           </div>
         </div>
