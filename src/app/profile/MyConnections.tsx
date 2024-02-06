04d1f19c8f155899ec651a0167f8e8cef457e22e
Mohammed Falah <falahsss900@gmail.com>
2024-02-06 17:58:56 +0530

    added my connections - profile

---
+++ src/app/profile/MyConnections.tsx
@@ -0,0 +1,400 @@
+"use client";
+
+import { TooltipWrapper } from "@/components/TooltipWrapper";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { connectionType } from "@/lib/types";
+import {
+  baseURL,
+  basicAuth,
+  findCoachUID,
+  findCoacheeUID,
+  getUserAccount,
+} from "@/lib/utils";
+import { Check, Loader, LucideExternalLink, X } from "lucide-react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { toast } from "sonner";
+
+const MyComnnections = ({ user }: any) => {
+  const router = useRouter();
+  const [connectionsForCoach, setConnectionsForCoach] = useState<
+    connectionType[]
+  >([]);
+  const [connectionsForCoachee, setConnectionsForCoachee] = useState<
+    connectionType[]
+  >([]);
+
+  const [userId, setUserId] = useState("");
+  const [loading, setLoading] = useState(true);
+
+  const getConnectionsForCoach = (coachId: string) => {
+    setLoading(true);
+    fetch(
+      `${baseURL}/accounts/coach-coachee-connections/?coach_id=${coachId}`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: basicAuth,
+        },
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+
+        setConnectionsForCoach(data.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+      });
+  };
+
+  const getConnectionsForCoachee = (coacheeId: string) => {
+    fetch(
+      `${baseURL}/accounts/coach-coachee-connections/?coachee_id=${coacheeId}`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: basicAuth,
+        },
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setConnectionsForCoachee(data.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    getUserAccount(user)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setUserId(data.uid);
+        fetch(
+          `${baseURL}/accounts/coach-coachee-mentor-mentee-profile/?user_id=${data.uid}`,
+          {
+            method: "GET",
+            headers: {
+              Authorization: basicAuth,
+            },
+          }
+        )
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+
+            const isApprovedData = data.data.filter(
+              (coachData: any) => coachData.is_approved === true
+            );
+
+            console.log(isApprovedData);
+            if (findCoacheeUID(isApprovedData).length > 0) {
+              console.log("for coachees");
+              getConnectionsForCoachee(findCoacheeUID(isApprovedData));
+            }
+
+            if (findCoachUID(isApprovedData).length > 0) {
+              console.log("for coaches");
+              getConnectionsForCoach(findCoachUID(isApprovedData));
+            }
+            setLoading(false);
+          })
+          .then((err) => {
+            console.error(err);
+            setLoading(false);
+          });
+      });
+  }, []);
+
+  const AcceptRequestComponent = ({
+    coachId,
+    coacheeId,
+  }: {
+    coachId: string;
+    coacheeId: string;
+  }) => {
+    const [acceptLoading, setAcceptLoading] = useState(false);
+    const acceptRequestHandler = () => {
+      setAcceptLoading(true);
+
+      fetch(`${baseURL}/accounts/coach-coachee-connections/`, {
+        method: "PATCH",
+        headers: {
+          Authorization: basicAuth,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          coach_id: coachId,
+          coachee_id: coacheeId,
+          status: "accepted",
+        }),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          toast.success("Accepted the request!");
+          getConnectionsForCoach(coachId);
+          setAcceptLoading(false);
+        })
+        .catch((err) => {
+          toast.error("Error accepting the request!");
+          console.error(err);
+          setAcceptLoading(false);
+        });
+    };
+    return (
+      <Button
+        onClick={() => {
+          acceptRequestHandler();
+        }}
+        variant={"outline"}
+        className="h-6 text-xs w-fit text-green-600 bg-green-50 border-green-300 "
+      >
+        <span className="max-sm:hidden">
+          {acceptLoading ? "Accepting..." : "Accept"}
+        </span>{" "}
+        <TooltipWrapper
+          className="hidden max-sm:block text-xs"
+          tooltipName="Accept"
+          body={<Check className="h-4 w-4 ml-2 max-sm:ml-0" />}
+        />
+      </Button>
+    );
+  };
+
+  const RejectRequestComponent = ({
+    coachId,
+    coacheeId,
+  }: {
+    coachId: string;
+    coacheeId: string;
+  }) => {
+    const [rejectLoading, setRejectLoading] = useState(false);
+    const rejectRequestHandler = () => {
+      setRejectLoading(true);
+      fetch(`${baseURL}/accounts/coach-coachee-connections/`, {
+        method: "PATCH",
+        headers: {
+          Authorization: basicAuth,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          coach_id: coachId,
+          coachee_id: coacheeId,
+          status: "rejected",
+        }),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          toast.success("Rejected the request!");
+          getConnectionsForCoach(coachId);
+          setRejectLoading(false);
+        })
+        .catch((err) => {
+          toast.error("Error rejecting the request!");
+          console.error(err);
+          setRejectLoading(false);
+        });
+    };
+    return (
+      <Button
+        onClick={() => {
+          rejectRequestHandler();
+        }}
+        variant={"outline"}
+        className="h-6 text-xs w-fit  text-red-600 font-semibold bg-red-50 border-red-300 "
+      >
+        <span className="max-sm:hidden">
+          {rejectLoading ? "Rejecting..." : "Reject"}
+        </span>{" "}
+        <TooltipWrapper
+          className="hidden max-sm:block text-xs"
+          tooltipName="Reject"
+          body={<X className="h-4 w-4 ml-2 max-sm:ml-0" />}
+        />
+      </Button>
+    );
+  };
+
+  return (
+    <div className="bg-accent p-2 mt-2 mb-8 rounded-md">
+      <div className="pl-4 max-sm:pl-2 pt-2">My Connections</div>
+
+      {!loading && (
+        <>
+          {connectionsForCoach.length === 0 &&
+            connectionsForCoachee.length === 0 && (
+              <>
+                <div className="text-xs w-full h-20 flex items-center justify-center">
+                  <div>You don't have any connections yet!</div>{" "}
+                </div>
+              </>
+            )}
+        </>
+      )}
+
+      {!loading ? (
+        <>
+          <div className="m-4 text-sm max-sm:m-0">
+            {connectionsForCoach.length > 0 && (
+              <div className="m-4 text-sm max-sm:m-0 max-sm:my-4">
+                <Badge>Connection Requests | as Coach</Badge>
+                <div className="bg-gray-200 px-4  max-sm:px-1 text-sm w-full m-2 ml-0 p-2 rounded-md">
+                  <div className="mx-4 flex flex-row my-3 gap-3 max-sm:gap-2  items-center max-sm:text-xs text-gray-600 font-semibold  max-sm:mx-1">
+                    <div className="w-[10%] max-sm:text-xs  text-center">
+                      SL no.
+                    </div>
+                    <div className=" w-[20%] text-center ">Name </div>
+                    <div className=" w-[40%]  text-center ">Status </div>
+                  </div>
+                  <div className="mx-4 max-sm:mx-1">
+                    {connectionsForCoach.map((connection, i) => (
+                      <div className="flex flex-row gap-3 my-2 items-center max-sm:justify-between">
+                        <p className="w-[10%] max-sm:w-[12%] text-center">
+                          {i + 1}
+                        </p>{" "}
+                        <p className="max-sm:text-xs w-[20%]  text-center">
+                          {connection.coachee_name}
+                        </p>{" "}
+                        {/* <div className="text-gray-400 bg-gray-400 h-5 w-[2px]" /> */}
+                        <div className="max-sm:text-xs w-[40%] flex flex-row gap-2 items-center justify-center  text-center">
+                          {connection.status === "pending" && (
+                            <>
+                              <AcceptRequestComponent
+                                coachId={connection.coach_id}
+                                coacheeId={connection.coachee_id}
+                              />
+                              <RejectRequestComponent
+                                coachId={connection.coach_id}
+                                coacheeId={connection.coachee_id}
+                              />
+                            </>
+                          )}
+                          {connection.status === "accepted" && (
+                            <Badge className="bg-green-400 hover:bg-green-300">
+                              Accepted
+                            </Badge>
+                          )}
+                          {connection.status === "rejected" && (
+                            <Badge className="bg-red-400 hover:bg-red-300">
+                              Rejected
+                            </Badge>
+                          )}
+                        </div>
+                        <div className="text-gray-400 bg-gray-400 h-5 w-[2px]" />
+                        <Button
+                          onClick={() => {
+                            router.push(
+                              `/?coachee_id=${connection.coachee_id}`
+                            );
+                          }}
+                          variant={"outline"}
+                          className="h-6 text-xs w-fit bg-gray-50 border-gray-300 "
+                        >
+                          <span className="max-sm:hidden">Visit Profile</span>{" "}
+                          <TooltipWrapper
+                            className="hidden max-sm:block text-xs"
+                            tooltipName="Visit Profile"
+                            body={
+                              <LucideExternalLink className="h-3 w-3 ml-2 max-sm:ml-0" />
+                            }
+                          />
+                        </Button>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            )}
+
+            {connectionsForCoachee.length > 0 && (
+              <div className="m-4 text-sm max-sm:m-0 max-sm:my-4">
+                <Badge>Connections | as Coachee </Badge>
+                <div className="bg-gray-200 px-4  max-sm:px-1  text-sm w-full m-2 ml-0 p-2 rounded-md">
+                  <div className="mx-4 flex flex-row my-3 gap-3  max-sm:gap-2 items-center max-sm:text-xs text-gray-600 font-semibold  max-sm:mx-1">
+                    <div className="w-[10%] max-sm:w-[12%] max-sm:text-xs  text-center">
+                      SL no.
+                    </div>
+                    <div className=" w-[20%] text-center ">Name </div>
+                    <div className=" w-[40%] text-center ">Status </div>
+                  </div>
+                  <div className="mx-4 max-sm:mx-1">
+                    {connectionsForCoachee.map((connection, i) => (
+                      <div className="flex flex-row gap-3 my-2 items-center max-sm:justify-between">
+                        <p className="w-[10%] max-sm:w-[12%] text-center">
+                          {i + 1}
+                        </p>{" "}
+                        <p className="max-sm:text-xs w-[20%]  text-center">
+                          {connection.coach_name}
+                        </p>{" "}
+                        <div className="max-sm:text-xs w-[40%]  text-center">
+                          {connection.status === "pending" && (
+                            <Badge className="bg-blue-400 hover:bg-blue-300">
+                              Pending
+                            </Badge>
+                          )}
+                          {connection.status === "accepted" && (
+                            <Badge className="bg-green-400 hover:bg-green-300">
+                              Accepted
+                            </Badge>
+                          )}
+                          {connection.status === "rejected" && (
+                            <Badge className="bg-red-400 hover:bg-red-300">
+                              Rejected
+                            </Badge>
+                          )}
+                        </div>
+                        <div className="text-gray-400 bg-gray-400 h-5 w-[2px]" />
+                        <Button
+                          disabled={connection.status !== "accepted"}
+                          formTarget="_blank"
+                          variant={"outline"}
+                          className="h-6 text-xs w-fit bg-gray-50 border-gray-300 "
+                        >
+                          <Link
+                            className="flex w-fit flex-row items-center justify-center"
+                            target="_blank"
+                            href={`/coach/${connection.coach_avatar_bot_id}`}
+                          >
+                            <span className="max-sm:hidden">Visit Coach</span>{" "}
+                            <TooltipWrapper
+                              className="hidden max-sm:block text-xs"
+                              tooltipName="Visit Coach"
+                              body={
+                                <LucideExternalLink className="h-3 w-3 ml-2 max-sm:ml-0" />
+                              }
+                            />
+                          </Link>
+                        </Button>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            )}
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="text-xs w-full h-20 flex items-center justify-center">
+            <div>
+              <Loader className="h-4 w-4 mr-2 animate-spin inline" /> Loading
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default MyComnnections;
