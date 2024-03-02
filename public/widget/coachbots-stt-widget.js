ee647ea4af4f5c6fc5984b02625a08f13346fd72
Aadil <ansariaadil611@gmail.com>
2024-03-02 12:55:30 +0530

    fixed mistake

--- public/widget/coachbots-stt-widget.js
+++ public/widget/coachbots-stt-widget.js
@@ -2,8 +2,8 @@ const key2 = "";
 const secret2 = "";
 
 const subdomainStt = window.location.hostname.split(".")[0];
-// const devUrlStt = "https://coach-api-ovh.coachbots.com/api/v1";
-const devUrlStt = "http://127.0.0.1:8001/api/v1"
+const devUrlStt = "https://coach-api-ovh.coachbots.com/api/v1";
+// const devUrlStt = "http://127.0.0.1:8001/api/v1"
 // const devUrlStt = "https://coach-api-gcp.coachbots.com/api/v1";
 const prodUrlStt = "https://coach-api-prod-ovh.coachbots.com/api/v1";
 const baseURL2 = subdomainStt === "platform" ? prodUrlStt : devUrlStt;
