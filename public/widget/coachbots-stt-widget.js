5f753b515ae7f50ce74611eec294b664de23cce1
Aadil <ansariaadil611@gmail.com>
2024-03-19 16:45:34 +0530

    implemented

--- public/widget/coachbots-stt-widget.js
+++ public/widget/coachbots-stt-widget.js
@@ -134,6 +134,7 @@ let endSessionButton;
 let intakeButton;
 let isAnonymous = false;
 let UserProfileInfo;
+let sessionQnAdata = [];
 
 // sample recommendation data
 let recommendationsDataStt = [
@@ -1968,11 +1969,12 @@ function sendBotTranscript2() {
       fetch(
         `${baseURL2}/test-attempt-sessions/send-bot-transcript-email/?${queryParamsEmail2}`,
         {
-          method: "GET",
+          method: "POST",
           headers: {
             Authorization: `Basic ${createBasicAuthToken2(key2, secret2)}`,
             "Content-Type": "application/json",
           },
+          body: JSON.stringify({ "session_qna_data": sessionQnAdata }),
         }
       )
         .then((response) => response.json())
@@ -4505,7 +4507,7 @@ loadExternalModule().then(() => {
     return audioCont;
   };
 
-  const anthropicAiResponse = (userInputMessage, signals, conversationId) => {
+  const anthropicAiResponse = (userInputMessage, signals, conversationId, latestMessage) => {
     const messageNode = document.createElement("div");
     messageNode.classList.add("inner-message-container");
 
@@ -4557,6 +4559,10 @@ loadExternalModule().then(() => {
             }
           });
 
+          // add user question and bot answer to the session
+          sessionQnAdata.push({"user": latestMessage, "coach": messageText.innerText});
+          console.log("sessionQnAdata :", sessionQnAdata);
+
           fetch(`${baseURL2}/coaching-conversations/save-ai-response/`, {
             method: "POST",
             headers: {
@@ -4574,6 +4580,21 @@ loadExternalModule().then(() => {
             })
             .catch((err) => {
               console.error(err);
+              fetch(`${baseURL2}/coaching-conversations/save-ai-response/`, {
+                method: "POST",
+                headers: {
+                  Authorization: `Basic ${createBasicAuthToken2(key2, secret2)}`,
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                  ai_response: messageText.innerText,
+                  conversation_id: conversationId,
+                }),
+              })
+                .then((response) => response.json())
+                .then((data) => {
+                  console.log(data);
+                })
             });
 
           return;
@@ -4586,7 +4607,7 @@ loadExternalModule().then(() => {
     });
   };
 
-  const OpenAiResponse = (userInputMessage, signals, conversationId) => {
+  const OpenAiResponse = (userInputMessage, signals, conversationId, latestMessage) => {
     const messageNode = document.createElement("div");
     messageNode.classList.add("inner-message-container");
 
@@ -4610,6 +4631,8 @@ loadExternalModule().then(() => {
 
     const shadowRoot = document.getElementById("chat-element2").shadowRoot;
     const allMessages = shadowRoot.getElementById("messages").childNodes;
+    sessionQnAdata.push({"user": latestMessage, "coach": messageText.innerText});
+    console.log("sessionQnAdata :", sessionQnAdata);
 
     fetch("/api/openai", {
       method: "POST",
@@ -4654,7 +4677,19 @@ loadExternalModule().then(() => {
               console.log(data);
             })
             .catch((err) => {
-              console.error(err);
+              console.error("Error occured : ", err);
+
+              fetch(`${baseURL2}/coaching-conversations/save-ai-response/`, {
+                method: "POST",
+                headers: {
+                  Authorization: `Basic ${createBasicAuthToken2(key2, secret2)}`,
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                  ai_response: messageText.innerText,
+                  conversation_id: conversationId,
+                }),
+              })
             });
 
           return;
@@ -5155,7 +5190,8 @@ loadExternalModule().then(() => {
               anthropicAiResponse(
                 responseData.coach_message_metadata.prompt,
                 signals,
-                conversation_id2
+                conversation_id2,
+                latestMessage
               );
               conversation_id2 = responseData["uid"];
               let coachResponse = responseData["coach_message_text"];
