60e328f777491ee923b89b3e0f28a432d7a7e62d
Mohammed Falah <falahsss900@gmail.com>
2024-06-08 10:50:45 +0530

    Update next.config.mjs : remove console

--- next.config.mjs
+++ next.config.mjs
@@ -1,44 +1,30 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//     webpack: (config) => {
-//       config.externals.push({
-//         sharp: "commonjs sharp",
-//         canvas: "commonjs canvas",
-//       });
-//       return config;
-//     },
-//     env: {
-//           OPENAI_API_KEY : process.env.NEXT_PUBLIC_OPENAI_API_KEY,
-//           ANTHROPIC_API_KEY : process.env.NEXT_PUBLIC_ANTHROPIC_API_KEY,
-//         },
-//   };
-  
-//   export default nextConfig;
-
-
-
-
-  /** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
 
 // dotenv
-import dotenv from "dotenv"
+import dotenv from "dotenv";
+import os from "os";
 
-dotenv.config()
+dotenv.config();
+
+const hostname = os.hostname()
 
 const nextConfig = {
   reactStrictMode: true,
   webpack: (config) => {
-          config.externals.push({
-            sharp: "commonjs sharp",
-            canvas: "commonjs canvas",
-          });
-          return config;
-        },
-        env: {
-              OPENAI_API_KEY : process.env.NEXT_PUBLIC_OPENAI_API_KEY,
-              ANTHROPIC_API_KEY : process.env.NEXT_PUBLIC_ANTHROPIC_API_KEY,
-              GEMINI_API_KEY : process.env.NEXT_PUBLIC_GEMINI_API_KEY
-            },
-}
+    config.externals.push({
+      sharp: "commonjs sharp",
+      canvas: "commonjs canvas",
+    });
+    return config;
+  },
+  env: {
+    OPENAI_API_KEY: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
+    ANTHROPIC_API_KEY: process.env.NEXT_PUBLIC_ANTHROPIC_API_KEY,
+    GEMINI_API_KEY: process.env.NEXT_PUBLIC_GEMINI_API_KEY,
+  },
+  compiler : {
+    removeConsole : hostname.includes("platform") ? { exclude: ["error", "warn"] } : false
+  }
+};
 
-export default nextConfig
+export default nextConfig;
