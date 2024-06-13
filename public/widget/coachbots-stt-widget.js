4d6d868a3900ac713d9afec027172fd2b6cf2fc0
falahh6 <falahsss900@gmail.com>
2024-06-13 14:09:46 +0530

    begin session color

--- public/widget/coachbots-stt-widget.js
+++ public/widget/coachbots-stt-widget.js
@@ -3432,7 +3432,7 @@ function disableOrEnableButtons(id, is_disable = true) {
 function addStickerToMessage(sticker, msg, color='#3b82f6') {
   
   const divWithLabel = `<div style="display: flex; flex-direction: column; margin: 0; padding: 0;">
-  <div style="font-size : 12px; font-weight: bold; color: black; padding: 4px; border-radius:4px; width: fit-content; padding: 2px 8px; border-radius: 4px; border: 1px solid lightgray;">${sticker}</div>
+  <div style="font-size : 12px; font-weight: bold; color: ${sticker === "Begin Session" ? "white" : "black"}; padding: 4px; border-radius:4px; width: fit-content; padding: 2px 8px; border-radius: 4px; border: 1px solid lightgray; background-color : ${sticker === "Begin Session" ? "#21C55D" : "transparent"}">${sticker}</div>
   <div style="margin-top : 8px; padding-top: 0px;">${msg}</div>
   </div>`;
   return divWithLabel;
