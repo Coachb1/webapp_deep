ac621e667c458ce0ded2fe9bd2152a39ba630d3b
bagoriarajan <bagoriarajan@gmail.com>
2025-08-12 16:39:46 +0530

    udpated model order

--- src/app/api/openai/route.ts
+++ src/app/api/openai/route.ts
@@ -5,10 +5,10 @@ export async function POST(req: Request) {
   const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
   });
-  const { userInput } = await req.json();
+  const { userInput, selectedModel= "gpt-4-turbo"} = await req.json();
 
   const response = await openai.chat.completions.create({
-    model: "gpt-4-turbo",
+    model:  selectedModel ,
     max_tokens: 1000,
     messages: [{ role: "user", content: userInput }],
     stream: true,
